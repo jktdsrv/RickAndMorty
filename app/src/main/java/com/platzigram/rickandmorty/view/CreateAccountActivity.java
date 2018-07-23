09937586ad84a161ee86f9461891f0d2fca8600e
jktdsrv <jktdsrv@gmail.com>
2018-07-22 19:42:08 -0500

    First Commit

---
+++ app/src/main/java/com/platzigram/rickandmorty/view/CreateAccountActivity.java
@@ -0,0 +1,26 @@
+package com.platzigram.rickandmorty.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.platzigram.rickandmorty.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_title_createaccount),false);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
+
+}
